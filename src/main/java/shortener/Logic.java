eea003cdcdeec80a953e8c43dddfdcf13bce17c2
astomusic <astomusic@naver.com>
2014-09-26 21:56:21 +0900

    redis로 적용 완료

--- src/main/java/shortener/Logic.java
+++ src/main/java/shortener/Logic.java
@@ -5,117 +5,75 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
- 
+
+import redis.clients.jedis.Jedis;
+
 public class Logic {
-    public Connection getConnection() throws IllegalAccessException,
-        ClassNotFoundException, SQLException {
-    //adjust it
-    String url = "jdbc:mysql://localhost/url_shortener";
-    Class.forName("com.mysql.jdbc.Driver");
-    Connection conn = DriverManager.getConnection(url, "urlshortener", "db1004");
-    return conn;
-    }
- 
-    public String getId(String longUrl) throws Exception {
-    Connection conn = null;
-    ResultSet rs = null;
-    Statement st = null;
- 
-    String query = "SELECT id FROM url_data WHERE long_url='"
-        + longUrl.trim() + "'";
-    String id = null;
-    try {
-        try {
-        conn = getConnection();
-        st = conn.createStatement();
-        rs = st.executeQuery(query);
-        if (rs.next()) {
-            id = rs.getString("id");
-        }
-        } finally {
- 
-        if (rs != null) {
-            rs.close();
-        }
-        if (st != null) {
-            st.close();
-        }
-        if (conn != null) {
-            conn.close();
-        }
-        }
-    } catch (Exception e) {
-        throw e;
-    }
-    return id;
-    }
- 
-    public String getShort(String serverName, int port, String contextPath,
-        String longUrl) throws Exception {
- 
-    Connection conn = null;
- 
-    Statement st = null;
-    String id = getId(longUrl);// check if URL has been shorten already
-    if (id != null) {
-        // if id is not null, this link has been shorten already.
-        // nothing to do
- 
-    } else {
-        // at this point id is null, make it shorter
-        String sqlInsert = "INSERT INTO url_data(long_url) VALUES('"
-            + longUrl.trim() + "')";
-        try {
-        conn = getConnection();
-        st = conn.createStatement();
-        st.execute(sqlInsert);
-        } finally {
-        if (st != null) {
-            st.close();
-        }
-        if (conn != null) {
-            conn.close();
-        }
-        }
-        // after we insert the record, we obtain the ID as identifier of our
-        // new short link
-        id = getId(longUrl);
- 
-    }
-    return "http://" + serverName + ":" + port + contextPath + "/" + id;
-    }
- 
-    public String getLongUrl(String urlId) throws Exception {
-    if (urlId.startsWith("/")) {
-        urlId = urlId.replace("/", "");
-    }
-    String query = "SELECT long_url FROM url_data where id=" + urlId;
-    String longUrl = null;
-    Connection conn = null;
-    ResultSet rs = null;
-    Statement st = null;
- 
-    try {
-        conn = getConnection();
-        st = conn.createStatement();
-        rs = st.executeQuery(query);
-        if (rs.next()) {
-        longUrl = rs.getString("long_url");
-        }
-    } finally {
- 
-        if (rs != null) {
-        rs.close();
-        }
-        if (st != null) {
-        st.close();
-        }
-        if (conn != null) {
-        conn.close();
-        }
-    }
- 
-    return longUrl;
-    }
- 
+	private Jedis jedis = new Jedis("10.73.45.75", 6379);
+	private int key;
+
+	public void initJedis() {
+		String StringKey = jedis.get("key");
+		if (StringKey == null) {
+			jedis.set("key", "0");
+			key = 0;
+		} else {
+			key = Integer.parseInt(StringKey);
+		}
+		
+		System.out.println("init하고 키를 받아온거다 " + key);
+	}
+
+	public void jedisSet() {
+		jedis.set(key + "", "hello~~~~~~~");
+		key++;
+		System.out.println("레디스 인덱스 초기화: "+jedis.get("0"));
+	}
+
+	public void getConnection() throws IllegalAccessException, ClassNotFoundException, SQLException {	
+		initJedis();
+	}
+
+	public String getId(String longUrl) throws Exception {
+		getConnection();
+		String tempQuery = jedis.get(longUrl.trim());
+		return tempQuery;
+	}
+
+	public String getShort(String serverName, int port, String contextPath, String longUrl) throws Exception {
+		getConnection();
+
+		Connection conn = null;
+
+		Statement st = null;
+		String id = getId(longUrl);// check if URL has been shorten already
+		if (id != null) {
+			// if id is not null, this link has been shorten already.
+			// nothing to do
+
+		} else {
+			jedis.set(key+"", longUrl.trim());
+			System.out.println("해당 Index에 대해 해당하는 long이 없어서 "+jedis.get(key+"")+ "키로 레디스에 저장");
+			id = key+"";
+			key++;
+			jedis.set("key", key+"");
+		}
+		System.out.println("여기로 줄여짐! "+ "http://" + serverName + ":" + port + contextPath + "/" + id);
+		return "http://" + serverName + ":" + port + contextPath + "/" + id;
+	}
+
+	public String getLongUrl(String urlId) throws Exception {
+		getConnection();
+		if (urlId.startsWith("/")) {
+			urlId = urlId.replace("/", "");
+		}
+		String query = "SELECT long_url FROM url_data where id=" + urlId;
+		System.out.println(urlId + "로 쇼튼된 URL을 받았으니 긴 URL을 찾으러간다");
+
+		String tempLong = jedis.get(urlId);
+
+		System.out.println("긴걸 찾음 " + tempLong);
+		return tempLong;
+	}
+
 }
